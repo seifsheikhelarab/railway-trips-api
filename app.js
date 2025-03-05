92d512a8f6f500e14ecaf3f7f4cfe0c25a218cd3
Seif Sheikhelarab <seifusama@gmail.com>
2025-03-05 16:19:31 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+
+import express from 'express';
+import bodyParser from 'body-parser';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI,
+    {serverSelectionTimeoutMS: 5000})
+    .then(() => console.log(`Connected to MongoDB Server on ${process.env.MONGODB_URI}`))
+    .catch(err => console.error(err));
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) });
